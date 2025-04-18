8ecde896477cc7e76d708fcff56e0ff23637edb1
Victor-Ndulue <ndulue.victor.c@gmail.com>
2025-04-18 13:49:35 +0100

    Completed api routing via proxy

--- ApiGatewayApp/ApiGatewayApp/Extensions/ServiceExtensions.cs
+++ ApiGatewayApp/ApiGatewayApp/Extensions/ServiceExtensions.cs
@@ -1,5 +1,12 @@
-﻿namespace ApiGatewayApp.Extensions;
+﻿using ApiGatewayApp.Configs;
+
+namespace ApiGatewayApp.Extensions;
 
 public static class ServiceExtensions
 {
+    public static void ConfigureProxyService(this IServiceCollection services)
+    {
+        services.AddReverseProxy()
+            .LoadFromMemory(ProxyConfig.GetRoutes(), ProxyConfig.GetClusters());
+    }
 }
