e32d33768c5e33290adf61fef97d0c6d1f506a37
TheCodeINN <emijere.richard@gmail.com>
2025-04-30 17:29:07 +0100

    Refactor Program.cs to enhance logging setup and improve service configuration

--- ApiGatewayApp/ApiGatewayApp/Program.cs
+++ ApiGatewayApp/ApiGatewayApp/Program.cs
@@ -1,31 +1,74 @@
 using ApiGatewayApp.Extensions;
+using ApiGatewayApp.Middleware;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+// Set up the bootstrap logger for startup
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-
-builder.Services.ConfigureProxyService();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Log startup information
+    Log.Information("Starting up ApiGateway");
+
+    Log.Information("Starting up ApiGateway");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Configure services
+    builder.Services.ConfigureProxyService();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // Configure OpenTelemetry
+    builder.AddTelemetry();
+
+    // Configure Serilog
+    builder.AddLogging();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }    // Add Serilog request logging
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value ?? "unknown");
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme ?? "unknown");
+            diagnosticContext.Set("RemoteIP", httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown");
+        };
+    });
+
+    app.UseHttpsRedirection();
+
+    // Add custom request logging middleware
+    app.UseMiddleware<RequestLoggingMiddleware>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapReverseProxy(); 
+    
+    Log.Information("Starting ApiGateway web host");
+    
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapReverseProxy();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "ApiGateway terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
